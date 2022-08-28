b5692b67b69c8a5643c19e5f8dbf7d06d19db3ce
Shubham Todkar <Shubham Todkar@LAPTOP-ESNIPQPM.mshome.net>
2022-08-28 17:45:56 +0530

    [Anay]:- 2nd change

--- src/Saple3/s2.java
+++ src/Saple3/s2.java
@@ -1,6 +1,7 @@
 package Saple3;
 
-public class s2 {
+public class s2 
+{
 public static void main(String[] args) {
 	System.out.println("welcone");
 }
---
+++ src/Saple3/s3.java
@@ -0,0 +1,8 @@
+package Saple3;
+
+public class s3 {
+	public static void main(String[] args) {
+		System.out.println("welcone");
+	}
+
+}
